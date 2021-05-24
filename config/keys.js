69a5609c59cb21d8dbd4a505bb2176de48c35f79
brysonLin247 <47481681+brysonLin247@users.noreply.github.com>
2021-05-24 20:54:15 +0800

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI:"mongodb+srv://ljj:123@cluster0-eeogr.mongodb.net/NI-people",
+    mongoURI:"mongodb+srv://xxx:xxx@cluster0-eeogr.mongodb.net/NI-people",
     secretOrKey:"secret"
 }
